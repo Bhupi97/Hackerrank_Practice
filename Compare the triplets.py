d9a027d90c2bb6ab65fd45e68c3ac4933ca304f2
Bhupinder Partap Singh <bhupinderpartap97@gmail.com>
2020-03-14 18:25:50 +0530

    Create Compare the triplets.py

---
+++ Compare the triplets.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    x = 0
+    y = 0
+    for i in range(len(a)):
+        if(a[i] > b[i]):
+            x=x+1
+        if(a[i] < b[i]):
+            y=y+1
+        z = [x,y]
+
+    return z
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
